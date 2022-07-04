fc8d30cfe31ebf353bf8baaa807ddbe2322f222b
Jiny <jiny8510@gmail.com>
2022-07-04 14:08:49 +0900

    footer수정

--- src/components/Shoes.jsx
+++ src/components/Shoes.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const FooterWrap = styled.div`
   width: 100%;
-  height: 200px;
-  background-color: blue;
+  /* height: 200px; */
+  background-color: #f1f1e6;
+  line-height: 1.4;
 `;
 const Footer = styled.footer`
   max-width: 1440px;
@@ -15,7 +17,20 @@ export default function Shoes() {
   return (
     <FooterWrap>
       <Footer>
-        <h2>이 사이트는 포트폴리오입니다.</h2>
+        <p>이 사이트는 상업 목적이 아닌 포트폴리오용으로 제작되었습니다.</p>
+        <p>product by Zzoo & Jiny | © 2022 MovieApp, All rights reserved.</p>
+        <p>
+          Zzoo |
+          <a href="https://github.com/zzoodev" target="blank">
+            https://github.com/zzoodev
+          </a>
+        </p>
+        <p>
+          Jiny |
+          <a href="https://github.com/algojiny" target="blank">
+            https://github.com/algojiny
+          </a>
+        </p>
       </Footer>
     </FooterWrap>
   );
