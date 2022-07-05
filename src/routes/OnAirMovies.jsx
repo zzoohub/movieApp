f7a7ad12721bd9994b04b135e94b2f0d24b8a81a
Jiny <jiny8510@gmail.com>
2022-07-05 16:15:41 +0900

    OnAirMovies컴포넌트 생성 및 Nav경로 수정

---
+++ src/routes/OnAirMovies.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+`;
+export default function OnAirMovies() {
+  return <Wrapper></Wrapper>;
+}
